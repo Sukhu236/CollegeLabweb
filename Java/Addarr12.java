c08b7f0fbbb66d7767880b50fcf764c9c4edca09
Sukhu236 <83777280+Sukhu236@users.noreply.github.com>
2026-09-26 08:50:10 +0000

    add arr 12 matrix

---
+++ Java/Addarr12.java
@@ -0,0 +1,34 @@
+package Java;
+import java.util.Scanner;
+
+public class Addarr12 {
+    public static void main(String[] args) {
+
+        Scanner nub = new Scanner(System.in);
+        int[] arr1 = {10,20,30,40,50,60,70,80};
+        int[] arr2 = {10,20,30,40,50,60,70,80};
+        int[] sum = {} ;
+    
+    
+
+            for(int i = 0; i < arr1.length; i ++){
+
+                for(int j = 0; j < arr2.length; j ++){
+
+
+                    sum[]= arr1[i] + arr2[j];
+
+                    System.out.println(sum);
+
+                }
+
+                
+
+
+
+            }
+
+
+    }
+    
+}
